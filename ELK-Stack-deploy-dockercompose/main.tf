f31ce6629cf9191cacf411b51ed405cc58249494
epalencia9 <120271561+epalencia9@users.noreply.github.com>
2023-03-18 15:55:10 -0500

    Update main.tf

--- ELK-Stack-deploy-dockercompose/main.tf
+++ ELK-Stack-deploy-dockercompose/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "docker_server" {
-  ami             = "ami-060d3509162bcc386"
+  ami             = "ami-0df24e148fdb9f1d8"
   instance_type   = "t2.micro"
   key_name = "Natalierose"
   security_groups = ["default"]
